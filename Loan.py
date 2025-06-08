3c80b970730d6f6aacf71e96394e280865e02eff
lyz4r <aid19.02.06@mail.ru>
2025-06-08 19:05:28 +0300

    added loan module and main with system simulation

---
+++ Loan.py
@@ -0,0 +1,32 @@
+# Loan.py
+
+from datetime import date
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from User import Customer
+
+
+class Loan:
+    def __init__(self, id: int, amount: float, rate_interest: float, term: date, status: str, customer: 'Customer'):
+        self.id = id
+        self.amount = amount
+        self.rate_interest = rate_interest
+        self.term = term
+        self.status = status
+        self.customer = customer
+
+    def calculate(self) -> float:
+        days_passed = (date.today() - self.term).days
+        interest = self.amount * self.rate_interest * (days_passed / 365)
+        total_debt = self.amount + interest
+        return total_debt
+
+    def repay(self, sum: float) -> float:
+        if self.amount >= sum:
+            self.amount -= sum
+            print(f"Repaid {sum} from loan {self.id}")
+            return sum
+        else:
+            print("Cannot repay more than the remaining loan amount")
+            return 0
